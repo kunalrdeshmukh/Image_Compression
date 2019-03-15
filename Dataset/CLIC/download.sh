484d009aed60a0a73596cc7c4d6685fb8323c43d
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-03-15 07:18:42 +0000

    + folders

---
+++ Dataset/CLIC/download.sh
@@ -0,0 +1,4 @@
+nohup wget https://data.vision.ee.ethz.ch/cvl/clic/professional_train.zip > /dev/null 2>&1 &
+nohup wget https://data.vision.ee.ethz.ch/cvl/clic/mobile_train.zip > /dev/null 2>&1 &
+nohup wget https://data.vision.ee.ethz.ch/cvl/clic/professional_valid.zip > /dev/null 2>&1 & 
+nohup wget https://data.vision.ee.ethz.ch/cvl/clic/mobile_valid.zip > /dev/null 2>&1 &
